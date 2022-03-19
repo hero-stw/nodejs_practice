69619950b2aab3e0de6002227aa983caa7e26036
Hero <16040582@vnu.edu.vn>
2022-03-19 16:06:06 +0700

    feat: Update Port

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
